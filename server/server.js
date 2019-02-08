033012a2822f8ee636e4575194de018248c66d84
Dennis Angeline <dangeline@live.com>
2019-02-07 23:34:00 -0800

    added sockets

--- server/server.js
+++ server/server.js
@@ -1,13 +1,39 @@
 const path = require('path');
-const publicPath = path.join(__dirname, '../public');
-
 const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
 
-var PORT = process.env.PORT || 3000;
+const publicPath = path.join(__dirname, '../public');
+const PORT = process.env.PORT || 3000;
+
 var app = express();
 app.use (express.static(publicPath));
 
-app.listen(PORT, () => {
+var server = http.createServer(app);
+var io = socketIO(server);
+
+io.on('connection', (socket) => {
+	console.log(`user connected`);
+
+	socket.emit('newMessage', {
+		from: 'mike@example.com',
+		text: 'Seee ya',
+		createdAt: 123
+	});
+
+	socket.on('createMessage', (message) => {
+		console.log(`Message created`, message);
+	})
+
+	socket.on('disconnect', () => {
+		console.log(`user disconnected`);
+	})
+})
+
+
+
+
+server.listen(PORT, () => {
 	console.log(`Started on port ${PORT}`);
 });
 
